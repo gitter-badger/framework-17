cf0f423fead5ecd41075e99cf6f3f64b3b1eccd2
Oytun OZDEMIR <oytunozdemir@gmail.com>
2015-12-21 22:13:12 +0200

    test update

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 require_once __DIR__.'/vendor/autoload.php';
-$bootstrap = new Bluejacket\Boot(array(
+new Bluejacket\Boot(array(
   "app" => "config/app.json",
   "database" => "config/database.json",
   "security" => "config/security.json",
   "types" => "config/types.json"
-))->dump();
+))::dump();
 ?>
