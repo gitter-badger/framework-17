8bfeba1bb17845aa563cc6edeaa1ef1017720b32
Oytun OZDEMIR <oytunozdemir@gmail.com>
2015-12-27 22:53:08 +0200

    Framework Updates

--- app/Controller/Home.php
+++ app/Controller/Home.php
@@ -5,6 +5,12 @@ class Home extends Controller
 {
     function index(){
         
+        $loader = new Twig_Loader_Filesystem('app/View/home');
+        $twig = new Twig_Environment($loader, array(
+            'cache' => 'app/Cache',
+        ));
+        $template = $twig->loadTemplate('index.html');
+        echo $template->render();
     }
 }
 ?>
