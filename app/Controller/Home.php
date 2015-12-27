6f218114279a7564d45fdf6940c520557f27a685
Oytun OZDEMIR <oytunozdemir@gmail.com>
2015-12-27 22:08:48 +0200

    Framework Updates

--- app/Controller/Home.php
+++ app/Controller/Home.php
@@ -3,8 +3,8 @@ namespace App\Controller;
 use Bluejacket\Core\Controller;
 class Home extends Controller
 {
-	function index(){
-		echo "Hello World! This is HomeController in app/Controller/Home.php";
-	}
+    function index(){
+        
+    }
 }
 ?>
--- app/route.php
+++ app/route.php
@@ -1,8 +1,4 @@
 <?php
 use Bluejacket\Core\Route;
 Route::root("GET","Home@index");
-
-Route::get('/_profiler',function(){
-	echo "Profiler Page";
-});
 ?>
