f2eaf6916fc04a192365ef4466e78bf2ecc66c6d
Oytun Özdemir (Oytunistrator) <oytunozdemir@gmail.com>
2016-02-20 01:06:42 +0200

    Improvments

--- app/controller/Home.php
+++ app/controller/Home.php
@@ -1,20 +1,10 @@
 <?php
 namespace App\Controller;
-use Bluejacket\Core\Controller;
-use Twig_Loader_Filesystem;
-use Twig_Environment;
 class Home extends Controller
 {
     function index(){
-        
-        $loader = new Twig_Loader_Filesystem('app/View/home');
-        $twig = new Twig_Environment($loader, array(
-            'cache' => false,
-        ));
-        $template = $twig->loadTemplate('homepage.html.twig');
-        echo $template->render(array(
-            "server" => $_SERVER
-        ));
+    	$this->twig()->loadTemplate('home/homepage.html.twig')->render(array(
+    		'server' => $_SERVER
+    	));
     }
 }
-?>
