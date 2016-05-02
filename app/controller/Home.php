3bd37d267762e479d02b7d14415ad005d5b6fe93
Oytun Özdemir <oytunozdemir@gmail.com>
2016-05-02 18:01:17 +0300

    framework updates

--- app/controller/Home.php
+++ app/controller/Home.php
@@ -2,8 +2,5 @@
 namespace App\Controller;
 class Home extends Controller {
 	function index() {
-		$this->twig()->loadTemplate('home/homepage.html.twig')->render(array(
-				'server' => $_SERVER,
-			));
 	}
 }
